e6aaf28ceff528b80d2062e724d36bba80c26f06
Mason Phillips <mjphillips2000@gmail.com>
2021-01-12 17:48:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,7 +2,9 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/reversename/:name' do
-    
+    @ns = params[:name].to_s
+    @backwords = @ns.reverse
+    "#{@backwords}"
   end
 
 end
